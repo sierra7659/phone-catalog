5c1215c42007de28427a092a42576adaf5187f2a
sierra <rudiazbern@gmail.com>
2019-05-25 18:09:19 +0200

    DEVELOP Add get method for images in back

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@
 var express = require("express");
 var app = express();
 
+var PATH_IMAGES = "/phone-catalog/images/";
+
 app.listen(4000, () => {
     console.log("Server running on port 4000");
 });
@@ -33,4 +35,32 @@ app.get("/phone-list", async (req, res, next) => {
 
         console.log(e);
     }
+});
+
+app.get("/image/:name", async (req,res, next) => {
+    console.log("Getting Phone Photo " + req.params.name);
+    var options = {
+        root: __dirname + PATH_IMAGES,
+        dotfiles: 'deny',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    };
+    try {
+
+       await res.sendFile(req.params.name,options);
+
+       console.log("Image sent successfully");
+
+    } catch(e) {
+
+        res.status(500).json({
+            e: e.toString()
+        });
+
+        console.log(e);
+    
+    }
+
 });
